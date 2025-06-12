8c4021cb0d7509e4b77d917a8a9c5b5a5229c021
DAN <davidalejandrp74@gmail.com>
2025-06-12 14:35:19 -0400

    fixin bug build

--- index.js
+++ index.js
@@ -407,7 +407,7 @@ app.post('/api/gemini/generate', authenticateToken, async (req, res) => {
         user: userData.id ? userData.id : "Desconocido",
         userdata: userData ? userData : "Desconocido",
 
-
+        env: "Creacion de leccion",
         status: 'success',
 
         description: "Creacion de curso",
@@ -554,7 +554,7 @@ Usa formato Markdown con encabezados, listas, código y otros elementos de forma
 
         user: user.id ? user.id : "Desconocido",
         userdata: user ? user : "Desconocido",
-
+        env: "Creacion de leccion",
         status: 'success',
         description: "Creacion de leccion",
         url: req.originalUrl,
